86e5660c92d3e024f8b5822878724ddb0bd021de
Parag Meshram <parag.meshram@hotmail.com>
2020-05-28 22:04:13 +0530

    Added browser-sync for accessing website from devices

--- config/webpack.dev.js
+++ config/webpack.dev.js
@@ -1,15 +1,25 @@
 const merge = require('webpack-merge');
 const common = require('./webpack.common.js');
+const BrowserSyncPlugin = require('browser-sync-webpack-plugin');
 
 module.exports = merge(common, {
     mode: 'development',
     devtool: 'eval-source-map',
     devServer: {
-        host: '0.0.0.0',
         historyApiFallback: true,
         overlay: {
             warnings: true,
             errors: true
         },
-    }
+        hot: false,
+        inline: false,
+    },
+    plugins: [
+        new BrowserSyncPlugin({
+            host: 'localhost',
+            port: 3000,
+            proxy: 'http://localhost:8080/',
+            reloadDelay: 0
+        })
+    ]
 });
